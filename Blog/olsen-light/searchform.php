26f00f39f11c69c2473b2ddad2764a4c135c4306
Willian Matiola <willianmatiola@MacBook-do-willianmatiola.local>
2020-07-15 20:13:25 -0300

    remove useless files

--- Blog/olsen-light/searchform.php
+++
@@ -1,7 +0,0 @@
-<form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="searchform" method="get" role="search">
-	<div>
-		<label class="screen-reader-text"><?php esc_html_e( 'Search for:', 'olsen-light' ); ?></label>
-		<input type="text" placeholder="<?php echo esc_attr_x( 'Search', 'search box placeholder', 'olsen-light' ); ?>" name="s" value="<?php echo get_search_query(); ?>">
-		<button class="searchsubmit" type="submit"><i class="fa fa-search"></i><span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'olsen-light' ); ?></span></button>
-	</div>
-</form>
--- Blog/olsen-light/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar sidebar-right" role="complementary" itemtype="http://schema.org/WPSideBar" itemscope="itemscope">
-	<?php
-		if( is_page() && is_active_sidebar( 'page' ) ) {
-			dynamic_sidebar( 'page' );
-		} else {
-			dynamic_sidebar( 'blog' );
-		}
-	?>
-</div><!-- /sidebar -->
